b044b8bf64fa23d44cc846b1510fa84ba28dbb4e
dustintktran <dustintktran@gmail.com>
2021-10-18 19:18:03 -0500

    Query and Item box added
    Added 2 component
    Modal currently will not close.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,39 @@
-import React, { FC } from 'react';
-import { Button } from 'antd';
+import React, { FC, useState, useEffect } from 'react';
+import { Button, Modal } from 'antd';
+import Query from './Query';
 
-const App: FC = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
 
+
+const App = () => {
+  const [isLookupVisible, setIsLookupVisible] = useState(false);
+
+  useEffect(() => {
+    console.log(isLookupVisible);
+  })
+  const openLookup = () => { 
+    setIsLookupVisible(true);
+  };
+  const handleOk = () => { 
+    setIsLookupVisible(false);
+  };
+  const handleCancel = () =>  {
+    setIsLookupVisible(false);
+  }
+  return (
+    <div className="App">
+      <Button onClick={openLookup}>Open Search Modal</Button>
+      <div>hello! {isLookupVisible} ===</div>
+      <Modal title="Search" visible={isLookupVisible} onOk={handleOk} onCancel={handleCancel} destroyOnClose={true} >
+        <Query />
+      </Modal>
+    </div>
+  )
+}
 export default App;
+
+/*
+footer={[
+  <Button onClick={closeLookup}> Close </Button>
+]}
+
+*/
---
+++ src/components/ItemBox.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+
+const ItemBox = () => (
+  <div className="PokeInfo">
+
+  </div>
+);
+
+export default ItemBox;
